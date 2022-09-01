d8ad9e66ce302cb0fce5a8cb07a9154bc4e37dca
Marco Potok <potok.marco@gmail.com>
2022-09-01 18:00:44 +0200

    Add javadocs

--- src/main/java/io/github/marcopotok/jpb/Clause.java
+++ src/main/java/io/github/marcopotok/jpb/Clause.java
@@ -9,12 +9,27 @@ public interface Clause {
     Clause CONJUNCTION = (criteriaBuilder, pathProvider) -> criteriaBuilder.conjunction();
     Clause DISJUNCTION = (criteriaBuilder, pathProvider) -> criteriaBuilder.disjunction();
 
+    /**
+     * Converts the clause to a {@link Predicate}
+     * @param criteriaBuilder
+     * @param pathProvider
+     * @return the predicate
+     */
     Predicate toPredicate(CriteriaBuilder criteriaBuilder, PathProvider pathProvider);
 
+    /**
+     * Concatenates two clauses with a logic AND
+     * @param clause
+     * @return the result clause
+     */
     default Clause and(Clause clause) {
         return ClauseComposition.composed(this, clause, CriteriaBuilder::and);
     }
 
+    /**
+     * Concatenates two clauses with a logic OR
+     * @return the result clause
+     */
     default Clause or(Clause clause) {
         return ClauseComposition.composed(this, clause, CriteriaBuilder::or);
     }
--- src/main/java/io/github/marcopotok/jpb/PredicateBuilder.java
+++ src/main/java/io/github/marcopotok/jpb/PredicateBuilder.java
@@ -33,22 +33,39 @@ public class PredicateBuilder<T> {
     private final Collection<String> prefetches = new LinkedList<>();
     private final PrefetchEngine prefetchEngine;
 
+    /**
+     * Initialize a {@link PredicateBuilder} with default prefetch engine
+     */
     public PredicateBuilder() {
         this(new DefaultPrefetchEngine());
     }
 
+    /**
+     * Initialize a {@link PredicateBuilder} with custom prefetch engine
+     */
     public PredicateBuilder(PrefetchEngine prefetchEngine) {
         this.prefetchEngine = prefetchEngine;
     }
 
+    /**
+     * Initialize a {@link PredicateBuilder} with default prefetch engine
+     */
     public static <T> PredicateBuilder<T> builder() {
         return new PredicateBuilder<>();
     }
 
+    /**
+     * Initialize a {@link PredicateBuilder} with custom prefetch engine
+     */
     public static <T> PredicateBuilder<T> of(Class<T> clazz) {
         return builder();
     }
 
+    /**
+     * Build the predicate
+     *
+     * @return the predicate created
+     */
     public Predicate build(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
         prefetches.forEach(prefetch -> prefetchEngine.prefetch(prefetch, root, query));
         return predicates.stream()
@@ -80,15 +97,32 @@ public class PredicateBuilder<T> {
         return this;
     }
 
+    /**
+     * Add a predicate on where clause for entity's id is equal to {@code id}
+     */
     public <U> PredicateBuilder<T> withId(U id) {
         return withProperty("id", id);
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is equal to {@code value}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param value If null, no filtering will be performed
+     */
     public <U> PredicateBuilder<T> withProperty(String name, U value) {
         addPredicateContextIfHasValue(name, Operators.EQUALS, value);
         return this;
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is equal to {@code value}.
+     * If {@code value} is null, a disjunction predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param value If null, a disjunction predicate will be added.
+     */
     public <U> PredicateBuilder<T> withRequiredProperty(String name, U value) {
         if (value == null) {
             disjunct();
@@ -97,32 +131,74 @@ public class PredicateBuilder<T> {
         return withProperty(name, value);
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is equal ignore case to {@code value}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param value If null, no filtering will be performed
+     */
     public <U extends String> PredicateBuilder<T> withPropertyIgnoreCase(String name, U value) {
         addPredicateContextIfHasValue(name, Operators.EQUALS_UPPER_CASE,
                 value != null ? value.toUpperCase(Locale.ROOT) : null);
         return this;
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is equal to {@code value}.
+     * If {@code value} is null or empty, no predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param value If null or empty, no filtering will be performed
+     */
     public <U extends String> PredicateBuilder<T> withRequiredProperty(String name, U value) {
         return withRequiredProperty(name, value == null || value.isEmpty() ? null : (Object) value);
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is not equal to {@code value}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param value If null, no filtering will be performed
+     */
     public <U> PredicateBuilder<T> withPropertyNot(String name, U value) {
         addPredicateContextIfHasValue(name, Operators.NOT_EQUALS, value);
         return this;
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is not equal ignore case to {@code value}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param value If null, no filtering will be performed
+     */
     public <U extends String> PredicateBuilder<T> withPropertyNotIgnoreCase(String name, U value) {
         addPredicateContextIfHasValue(name, Operators.NOT_EQUALS_UPPER_CASE,
                 value != null ? value.toUpperCase(Locale.ROOT) : null);
         return this;
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is in {@code values}.
+     * If {@code values} is null, no predicate will be added.
+     *
+     * @param name   name of the field of the entity to filter.
+     * @param values collection of values for a where clause. If null, no filtering will be performed
+     */
     public PredicateBuilder<T> withPropertyIn(String name, Collection<?> values) {
         addPredicateContextIfHasValue(name, Operators.IN, values);
         return this;
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is in {@code values}.
+     * If {@code values} is null or empty, a disjunction predicate will be added.
+     *
+     * @param name   name of the field of the entity to filter.
+     * @param values collection of values for a where clause. If null or empty, a disjunction predicate will be added.
+     */
     public PredicateBuilder<T> withRequiredPropertyIn(String name, Collection<?> values) {
         if (values == null || values.isEmpty()) {
             disjunct();
@@ -131,69 +207,133 @@ public class PredicateBuilder<T> {
         return withPropertyIn(name, values);
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is not in {@code values}.
+     * If {@code values} is null, no predicate will be added.
+     *
+     * @param name   name of the field of the entity to filter.
+     * @param values collection of values for a where clause. If null, no filtering will be performed
+     */
     public PredicateBuilder<T> withPropertyNotIn(String name, Collection<?> values) {
         addPredicateContextIfHasValue(name, Operators.NOT_IN, values);
         return this;
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is {@code null}.
+     *
+     * @param name name of the field of the entity to filter.
+     */
     public PredicateBuilder<T> withNullProperty(String name) {
         addPredicateContext(name, Operators.IS_NULL, null);
         return this;
     }
 
+    /**
+     * Add a predicate on where clause for entity's {@code name} is not {@code null}.
+     *
+     * @param name name of the field of the entity to filter.
+     */
     public PredicateBuilder<T> withNotNullProperty(String name) {
         addPredicateContext(name, Operators.NOT_NULL, null);
         return this;
     }
 
-    public PredicateBuilder<T> withPropertyLikeIgnoreCase(String propertyName, String propertyValue) {
-        if (propertyValue != null) {
-            addPredicateContextIfHasValue(propertyName, Operators.LIKE_UPPER_CASE,
-                    propertyValue.replaceAll(WILDCARD_REQUEST, WILDCARD_DB).toUpperCase(Locale.ROOT));
+    /**
+     * Add a predicate on where clause for entity's property {@code name} is like ignore case to {@code value}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param value If null, no filtering will be performed
+     */
+    public PredicateBuilder<T> withPropertyLikeIgnoreCase(String name, String value) {
+        if (value != null) {
+            addPredicateContextIfHasValue(name, Operators.LIKE_UPPER_CASE,
+                    value.replaceAll(WILDCARD_REQUEST, WILDCARD_DB).toUpperCase(Locale.ROOT));
         }
         return this;
     }
 
-    public PredicateBuilder<T> withPropertyStartingWith(String propertyName, String propertyValue) {
-        String likeValue = propertyValue == null ? WILDCARD_DB : propertyValue.toUpperCase(Locale.ROOT) + WILDCARD_DB;
-        addPredicateContextIfHasValue(propertyName, Operators.LIKE_UPPER_CASE, likeValue);
+    /**
+     * Add a predicate on where clause for entity's property {@code name} starts with {@code value}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param value If null, no filtering will be performed
+     */
+    public PredicateBuilder<T> withPropertyStartingWith(String name, String value) {
+        String likeValue = value == null ? WILDCARD_DB : value.toUpperCase(Locale.ROOT) + WILDCARD_DB;
+        addPredicateContextIfHasValue(name, Operators.LIKE_UPPER_CASE, likeValue);
         return this;
     }
 
-    public <U extends Comparable<? super U>> PredicateBuilder<T> withPropertyAfter(String propertyName, U from) {
-        addPredicateContextIfHasValue(propertyName, (value, path, cb) -> cb.greaterThan(path, value), from);
+    /**
+     * Add a predicate on where clause for entity's property {@code name} is greater than {@code from}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name name of the field of the entity to filter.
+     * @param from If null, no filtering will be performed
+     */
+    public <U extends Comparable<? super U>> PredicateBuilder<T> withPropertyAfter(String name, U from) {
+        addPredicateContextIfHasValue(name, (value, path, cb) -> cb.greaterThan(path, value), from);
         return this;
     }
 
-    public <X extends Comparable<? super X>> PredicateBuilder<T> withPropertyAfterInclusive(String propertyName,
-            X from) {
-        addPredicateContextIfHasValue(propertyName, (value, path, cb) -> cb.greaterThanOrEqualTo(path, value), from);
+    /**
+     * Add a predicate on where clause for entity's property {@code name} greater than or equal to {@code from}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name name of the field of the entity to filter.
+     * @param from If null, no filtering will be performed
+     */
+    public <X extends Comparable<? super X>> PredicateBuilder<T> withPropertyAfterInclusive(String name, X from) {
+        addPredicateContextIfHasValue(name, (value, path, cb) -> cb.greaterThanOrEqualTo(path, value), from);
         return this;
     }
 
-    public <X extends Comparable<? super X>> PredicateBuilder<T> withPropertyBefore(String propertyName, X to) {
-        addPredicateContextIfHasValue(propertyName, (value, path, cb) -> cb.lessThan(path, value), to);
+    /**
+     * Add a predicate on where clause for entity's property {@code name} is less than {@code from}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param to If null, no filtering will be performed
+     */
+    public <X extends Comparable<? super X>> PredicateBuilder<T> withPropertyBefore(String name, X to) {
+        addPredicateContextIfHasValue(name, (value, path, cb) -> cb.lessThan(path, value), to);
         return this;
     }
 
-    public <X extends Comparable<? super X>> PredicateBuilder<T> withPropertyBeforeInclusive(String propertyName,
+    /**
+     * Add a predicate on where clause for entity's property {@code name} is less than or equal to {@code from}.
+     * If {@code value} is null, no predicate will be added.
+     *
+     * @param name  name of the field of the entity to filter.
+     * @param to If null, no filtering will be performed
+     */
+    public <X extends Comparable<? super X>> PredicateBuilder<T> withPropertyBeforeInclusive(String name,
             X to) {
-        addPredicateContextIfHasValue(propertyName, (value, path, cb) -> cb.lessThanOrEqualTo(path, value), to);
+        addPredicateContextIfHasValue(name, (value, path, cb) -> cb.lessThanOrEqualTo(path, value), to);
         return this;
     }
 
+    /**
+     * Add a predicate on where clause for entity's property {@code name} with max value. 
+     */
     public <X extends Comparable<X>> PredicateBuilder<T> withPropertyMaxValue(Class<T> entityClass,
-            Class<X> propertyClass, String propertyName) {
+            Class<X> propertyClass, String name) {
         predicates.add((root, query, cb) -> {
             Subquery<X> subQuery = query.subquery(propertyClass);
             Root<T> subRoot = subQuery.from(entityClass);
-            Path<X> x = subRoot.get(propertyName);
+            Path<X> x = subRoot.get(name);
             subQuery.select(cb.greatest(x));
-            return cb.equal(root.get(propertyName), subQuery);
+            return cb.equal(root.get(name), subQuery);
         });
         return this;
     }
 
+    /**
+     * Add a predicate on where clause by the means of custom operator
+     */
     public <U> PredicateBuilder<T> with(U value, Function<U, Clause> operator) {
         Objects.requireNonNull(operator, "Operator cannot be null");
         if (value != null) {
@@ -202,6 +342,9 @@ public class PredicateBuilder<T> {
         return this;
     }
 
+    /**
+     * Add a predicate on where clause by the means of custom {@link Clause}
+     */
     public PredicateBuilder<T> with(Clause clause) {
         if (clause != null) {
             predicates.add((root, criteriaQuery, criteriaBuilder) -> clause.toPredicate(criteriaBuilder,
@@ -210,17 +353,23 @@ public class PredicateBuilder<T> {
         return this;
     }
 
-    public PredicateBuilder<T> groupBy(String... propertyName) {
+    /**
+     * Add a predicate with group by clause on {@code names}
+     */
+    public PredicateBuilder<T> groupBy(String... names) {
         predicates.add((root, query, criteriaBuilder) -> {
-            query.groupBy(Arrays.stream(propertyName).map(root::get).collect(Collectors.toList()));
+            query.groupBy(Arrays.stream(names).map(root::get).collect(Collectors.toList()));
             return criteriaBuilder.conjunction();
         });
         return this;
     }
 
-    public PredicateBuilder<T> project(String... propertyName) {
+    /**
+     * Add a predicate for selection only a subset of the entity's fields.
+     */
+    public PredicateBuilder<T> project(String... names) {
         predicates.add((root, query, criteriaBuilder) -> {
-            query.multiselect(Arrays.stream(propertyName).map(root::get).collect(Collectors.toList()));
+            query.multiselect(Arrays.stream(names).map(root::get).collect(Collectors.toList()));
             return criteriaBuilder.conjunction();
         });
         return this;
